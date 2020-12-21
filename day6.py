0afc969132337fe5374596ebe9eb304791c9bd7f
jesravnbol <jra@kapacity.dk>
2020-12-21 17:24:56 +0100

    Day 6, part 2 done.

--- day6.py
+++ day6.py
@@ -13,26 +13,35 @@ def get_data(filename: Path) -> List[str]:
 def parse_input(input: str) -> List[List[str]]:
     parsed_input = [s.strip("\n") for s in input]
     parsed_input = [
-        list(group) for k, group in groupby(parsed_input, lambda x: x == "")
-        if not k
+        list(group) for k, group in groupby(parsed_input, lambda x: x == "") if not k
     ]
     return parsed_input
 
 
-def get_yes_count_for_group(group_input: List[str]) -> int:
+def get_yes_count_for_group_part_1(group_input: List[str]) -> int:
     character_list = [c for s in group_input for c in s]
     return len(set(character_list))
 
 
-def get_answer_part_1(input_parsed):
-    return sum([get_yes_count_for_group(g) for g in input_parsed])
+def get_yes_count_for_group(group_input: List[str], version: str) -> int:
+    individual_character_sets = [set(s) for s in group_input]
+    if version == "part1":
+        set_of_yes_anwers = set.union(*individual_character_sets)
+    elif version == "part2":
+        set_of_yes_anwers = set.intersection(*individual_character_sets)
+    else:
+        raise ValueError("version must be 'part1' or 'part2")
+    return len(set_of_yes_anwers)
 
 
 def main():
     INPUT_PATH = Path("data/day6.txt")
     input = get_data(INPUT_PATH)
     parsed_input = parse_input(input)
-    print(f"Answer part 1: {get_answer_part_1(parsed_input)}")
+    answer_part_1 = sum([get_yes_count_for_group(g, "part1") for g in parsed_input])
+    answer_part_2 = sum([get_yes_count_for_group(g, "part2") for g in parsed_input])
+    print(f"Answer part 1: {answer_part_1}")
+    print(f"Answer part 2: {answer_part_2}")
 
 
 ##################################################################
@@ -41,7 +50,14 @@ def main():
 TEST_INPUT_PATH = Path("data/day6_test_part1.txt")
 test_input = get_data(TEST_INPUT_PATH)
 test_parsed_input = parse_input(test_input)
-assert get_answer_part_1(test_parsed_input) == 11
+test_answer_part_1 = sum(
+    [get_yes_count_for_group(g, "part1") for g in test_parsed_input]
+)
+assert test_answer_part_1 == 11
+test_answer_part_2 = sum(
+    [get_yes_count_for_group(g, "part2") for g in test_parsed_input]
+)
+assert test_answer_part_2 == 6
 
 
 if __name__ == "__main__":
